37ad0f7a0cd6068fd144d32b0a256e218c7f8694
Roman Chernyak <chernyak.roman@mail.ru>
2023-08-12 15:36:38 +0300

    base-functional

---
+++ src/App.vue
@@ -0,0 +1,166 @@
+<script>
+
+/*good
+parseEq
+despac
+isNumericChar*/
+
+export default{
+    data(){
+      return{
+        n1:'',
+        n2:'',
+        sn:'',
+        msg:'',
+        snaki:['-', '+', '/','*'],
+        res: ''
+      }
+    },
+    methods:{
+      add(){
+        switch(this.sn){
+          case'+':
+            return parseInt(this.n1) + parseInt(this.n2)
+          case'-':
+            return parseInt(this.n1) - parseInt(this.n2)
+          case'*':
+            return parseInt(this.n1) * parseInt(this.n2)
+          case'/':
+            return parseInt(this.n1) / parseInt(this.n2)
+          
+        }
+        
+      },
+      equal(){
+        this.msg = this.res
+      },
+
+      isNumericChar(c) { 
+        return /\d/.test(c)
+      },
+
+      parenthesisDivision(msg){
+      let stackOfIndex = []
+      let msgLen = msg.length
+        for (let i = 0; i<msgLen; i++){
+          if (msg[i] === '('){
+            stackOfIndex.push(i)
+          }else if (msg[i] === ')'){
+            const prevPos = stackOfIndex.pop()
+            if (prevPos===undefined){
+              return false 
+            }
+            const parsed = this.parseEq(msg.slice(prevPos+1,i))
+            msg = msg.slice(0,prevPos) + this.rightOrderOperation(parsed) + msg.slice(i+1,msg.length)
+            msgLen = msg.length
+            i-= prevPos+1
+          }
+        }        
+        if (stackOfIndex[0]){
+          return false
+        }
+        if (msg){
+          return msg
+        }
+        
+      },
+      parseEq(m){
+        let msg = m
+        
+        let isPrevNum = this.isNumericChar(msg[0]) || msg[0] === '-'
+        let parsedEq = []
+        if (isPrevNum){
+          parsedEq.push(msg[0])
+          msg = msg.substring(1)
+        }
+
+        for (let i of msg){
+          if(this.isNumericChar(i) === isPrevNum){
+            parsedEq[parsedEq.length-1] += i
+          }else{
+            parsedEq.push(i)
+          }
+          isPrevNum = this.isNumericChar(i)
+        }
+        return parsedEq
+      },
+      
+      rightOrderOperation(m){
+        let msg = m
+        let len = msg.length
+        for (let i = 0; i<len; i++){
+          
+            switch (msg[i]){
+              case '*':
+                msg[i-1] = parseInt(msg[i-1])*parseInt(msg[i+1])
+                msg.splice(i,2)
+                i -= 2
+                len = msg.length
+                break
+              case '/':
+                msg[i-1] = parseInt(msg[i-1])/parseInt(msg[i+1])
+                msg.splice(i,2)
+                i -= 2
+                len = msg.length
+                break
+            }
+            
+            
+            
+          
+        }
+        len = msg.length
+        for (let i = 0; i<len; i++){
+            switch (msg[i]){
+              case '+':
+                msg[i-1] = parseInt(msg[i-1]) + parseInt(msg[i+1])
+                msg.splice(i,2)
+                i -= 2
+                len = msg.length
+                break
+              case '-':
+                msg[i-1] = parseInt(msg[i-1]) - parseInt(msg[i+1])
+                msg.splice(i,2)
+                i -= 2
+                len = msg.length
+                break
+            
+            
+          }
+        }
+        return msg
+
+      },
+      solver(m){
+        let msg = m
+        if (msg){
+        msg = this.despacing(msg)
+        }
+        if(msg){
+          msg = this.parenthesisDivision(msg)
+        }
+        if(msg){
+        return msg
+        /*msg = this.parseEq(msg)
+        return this.rightOrderOperation(msg)*/
+        }
+      },
+      despacing(msg){
+        return msg.replace(/\s/g, '');
+      }
+
+    }
+
+}
+</script>
+
+<template>
+  <div>
+    <input v-model="msg">
+    
+  </div>
+  <button v-on:click="equal()">equals</button>
+  <h1>{{ solver(msg) }}</h1>
+</template>
+
+<style ></style>
